a6902d3b4fb73cc4f50fb8f97e38798e95d0be47
nanyona <anyonanicholas86@gmail.com>
2024-01-15 20:01:04 +0300

    register nn model in the admin interface

--- dnn/admin.py
+++ dnn/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django import forms
+from dnn.models import NNModel
+
+
+class NNForm(forms.ModelForm):
+    class Meta:
+        model = NNModel
+        exclude = ["nn"]
+
+
+class NNAdmin(admin.ModelAdmin):
+    list_display = ["coords", "nn"]
+    form = NNForm
+
+
+admin.site.register(NNModel, NNAdmin)
