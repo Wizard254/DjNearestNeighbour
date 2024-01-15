fbe37a40cc9ea9676b0a067c1f23844a14cd424e
nanyona <anyonanicholas86@gmail.com>
2024-01-15 08:32:18 +0300

    add test for the /nn/ endpoint

--- dnn/tests.py
+++ dnn/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
 # Create your tests here.
+
+# Test the /nn/ endpoint
+factory = APIRequestFactory()
+request = factory.post('/nn/', '2,2;-1,30;20,11;4,5')
+print(request.body)
