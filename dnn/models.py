1dccf0f07240b7ad2c730497b88703daf9af2832
nanyona <anyonanicholas86@gmail.com>
2024-01-15 19:54:17 +0300

    model nearest neighbour coordinates

--- dnn/models.py
+++ dnn/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class NNModel(models.Model):
+    # A database index will be created for this field
+    coords = models.CharField(db_index=True, unique=True)
+    nn = models.CharField()
+    modified = models.DateTimeField(auto_now=True)
+    pass
