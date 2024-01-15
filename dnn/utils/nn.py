4e9bb6e73a36431c425eee352a6cfb74aabd8f46
nanyona <anyonanicholas86@gmail.com>
2024-01-15 10:52:23 +0300

    parse coordinates from string

---
+++ dnn/utils/nn.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+import math
+
+
+@dataclass
+class Coord:
+    x: float
+    y: float
+    pass
+
+
+@dataclass
+class DistComparison:
+    c1: int
+    c2: int
+    d: float
+    pass
+
+
+def least(dcs: list[DistComparison]):
+    holder = []
+    for d in dcs:
+        holder.append(d.d)
+        pass
+    m = min(holder)
+    return [d for d in dcs if d == m]
+    pass
+
+
+def distance(a: Coord, b: Coord) -> float:
+    return math.fabs(a.x - b.x) + math.fabs(a.y - b.y)
+    pass
+
+
+def get_nn(coords: list[Coord]):
+    dcs = []
+    for i, c in enumerate(coords):
+        for j, cc in enumerate(coords):
+            if j == i:
+                continue
+                pass
+            dcs.append(DistComparison(i, j, distance(c, cc)))
+            pass
+        pass
+    return least(dcs)
+    pass
+
+
+def get_nn_str(s: str) -> (bool, list, str):
+    coords = s.split(';')
+    dcs = []
+    for c in coords:
+        cs = c.split(',')
+        if len(cs) == 2:
+            try:
+                dcs.append(Coord(float(cs[0]), float(cs[1])))
+                pass
+            except ValueError:
+                return False, None, "Invalid coordinate"
+                pass
+            pass
+        else:
+            return False, None, "Malformed coordinate"
+            pass
+        pass
+    if len(dcs) == 0:
+        return False, None, "No coordinates"
+        pass
+
+    return True, get_nn(dcs), None
+    pass
